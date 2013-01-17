1d834af5d523b77be89d9b633248f997560f33ee
Sukwon Oh <s3oh@s3oh-VirtualBox.(none)>
2013-01-17 16:15:00 -0500

    simple kernel bootstrapping

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,37 +1,52 @@
-;
-; Read some sectors from the boot disk using our disk_read function
-;
 [org 0x7c00]
+KERNEL_OFFSET equ 0x1000
 
-    mov [BOOT_DRIVE], dl
+	mov [BOOT_DRIVE], dl
 
-    mov bp, 0x8000
-    mov sp, bp
+	mov bp, 0x9000
+	mov sp, bp
 
-    mov bx, 0x9000
-    mov dh, 5
-    mov dl, [BOOT_DRIVE]
-    call disk_load
+	mov bx, MSG_REAL_MODE
+	call print_string
 
-    mov dx, [0x9000]
-    call print_hex
+	call load_kernel
 
-    mov dx, [0x9000 + 512]
-    call print_hex
+	call switch_to_pm
 
-    jmp $
+	jmp $
 
 %include "print_string.asm"
-%include "print_hex.asm"
 %include "disk_load.asm"
+%include "gdt.asm"
+%include "print_string_pm.asm"
+%include "switch_to_pm.asm"
 
-BOOT_DRIVE: db 0
+[bits 16]
+load_kernel:
+	mov bx, MSG_LOAD_KERNEL
+	call print_string
 
-;
-; Padding and magic BIOS number.
+	mov bx, KERNEL_OFFSET
+	mov dh, 15
+	mov dl, [BOOT_DRIVE]
+	call disk_load
 
-    times 510-($-$$) db 0
-    dw 0xaa55
+	ret
 
-    times 256 dw 0xdada
-    times 256 dw 0xface
+[bits 32]
+
+BEGIN_PM:
+	mov ebx, MSG_PROT_MODE
+	call print_string_pm
+
+	call KERNEL_OFFSET
+
+	jmp $
+
+BOOT_DRIVE db 0
+MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
+MSG_PROT_MODE db "Successfully landed in 32-bit Protected Mode", 0
+MSG_LOAD_KERNEL db "Loading kernel into memory", 0
+
+times 510-($-$$) db 0
+dw 0xaa55
